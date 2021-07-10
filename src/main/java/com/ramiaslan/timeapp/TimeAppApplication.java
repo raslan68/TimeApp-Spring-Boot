7a0ff551483db77331af9e89028a129024acef43
raslan68 <raslan68@yahoo.com>
2021-07-10 15:14:52 +0200

    initial project.

---
+++ src/main/java/com/ramiaslan/timeapp/TimeAppApplication.java
@@ -0,0 +1,13 @@
+package com.ramiaslan.timeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramiaslan/timeapp/TimeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramiaslan.timeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
